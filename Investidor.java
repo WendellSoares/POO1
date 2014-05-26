921d7ad01e9d6a464cecea83db5b7779571cdfb0
Aluno <me@me.com>
2014-05-26 11:35:07 -0300

    Primeiro commit

---
+++ Investidor.java
@@ -0,0 +1,14 @@
+
+public class Investidor extends Cliente{
+	private String empresa;
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+
+}
